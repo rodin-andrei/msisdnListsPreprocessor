9ccb72088f79078e7e40caaa3363d88f647f4577
Rainbovv <turcanu.radu17@gmail.com>
2021-03-17 14:21:20 +0200

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,10 @@
 import { Component } from '@angular/core';
-import {filesMap} from "./modules/drag-upload/drag-upload.component";
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent{
-
-
-  isCollapsed = false;
-
-  showDrag = false
-
-  showFiles = false
-
-  files:Map<string,string[]> = filesMap
-  title: string;
-
-
-
-  public showDragDiv() {
-    this.showFiles = false
-    this.showDrag = true
-
-  }
-  public showFilesDiv() {
-    this.showDrag = false
-    this.showFiles = true
-
-  }
+export class AppComponent {
+  title = 'ng-zorro2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {NzLayoutModule} from "ng-zorro-antd/layout";
-import {NzIconModule} from "ng-zorro-antd/icon";
-import {NzMenuModule} from "ng-zorro-antd/menu";
-import {DragUploadModule} from "./modules/drag-upload/drag-upload.module";
-
-
-registerLocaleData(en);
-
 
 @NgModule({
-
-    declarations: [
-        AppComponent,
-
-    ],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    NzMenuModule,
-    NzLayoutModule,
-    NzIconModule,
-    NzMenuModule,
-    DragUploadModule
+    AppRoutingModule
   ],
-    providers: [],
-    exports: [
-      // NzLayoutModule,
-      // NzLayoutComponent
-    ],
-    bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
